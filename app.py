00ecc96abf1ec475a67a2f0ced1950a5911e93ef
Madeleine Dunn <madeleine.dunn@arm.com>
2023-09-08 16:58:39 +0100

    Add gitignore and basic flask file

---
+++ app.py
@@ -0,0 +1,14 @@
+from markupsafe import escape
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return "<h1>Hello!</h1>"
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
