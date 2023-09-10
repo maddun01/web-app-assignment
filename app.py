516fa07a22745e4671904d5b9f6b64b01cf62e28
Madeleine Dunn <madeleine.dunn@arm.com>
2023-09-10 21:25:45 +0100

    Fix database relationships and add folders for structure

--- app.py
+++ app.py
@@ -22,61 +22,63 @@ class Device(db.Model):
     name = db.Column(db.Text)
     type = db.Column(db.Text)
     os = db.Column(db.Text)
-    # ONE TO ONE
-    ip = db.relationship("Ip", backref="device", uselist=False)
+    # MANY TO ONE
+    ip_id = db.Column(db.Integer, db.ForeignKey("ips.id"))
+    # ip = db.relationship("Ip", backref="device", uselist=False)
     date_added = db.Column(db.DateTime)
     last_run = db.Column(db.DateTime)
 
-    def __init__(self, name, type, os, date_added, last_run):
+    def __init__(self, name, type, os, ip_id, date_added, last_run):
         self.name = name
         self.type = type
         self.os = os
+        self.ip_id = ip_id
         self.date_added = date_added
         self.last_run = last_run
 
     def __repr__(self):
-        if self.ip:
-            return f"Device details: \n{self.name}\n{self.type}\n{self.os}\n{self.ip.name}\n{self.date_added}\n{self.last_run}"
-        else:
-            return f"Device details: \n{self.name}\n{self.type}\n{self.os}\nNo IP provided\n{self.date_added}\n{self.last_run}"
+        return f"Device details: \n{self.name}\n{self.type}\n{self.os}\n{self.ip_id}\n{self.date_added}\n{self.last_run}"
 
 
 class Ip(db.Model):
     __tablename__ = "ips"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
-    device_id = db.Column(db.Integer, db.ForeignKey("devices.id"))
+    # ONE TO MANY
+    device = db.relationship("Device", backref="ip", lazy="dynamic")
+    # device_id = db.Column(db.Integer, db.ForeignKey("devices.id"))
 
     def __init__(self, name, device_id):
         self.name = name
-        self.device_id = device_id
 
 
 class Network(db.Model):
     __tablename__ = "networks"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
-    # ONE TO MANY
-    datatype = db.relationship("Datatype", backref="network", lazy="dynamic")
+    # MANY TO ONE
+    datatype_id = db.Column(db.Integer, db.ForeignKey("datatypes.id"))
+    # datatype = db.relationship("Datatype", backref="network", lazy="dynamic")
     provenance = db.Column(db.Text)
     format = db.Column(db.Text)
     date_added = db.Column(db.DateTime)
     last_run = db.Column(db.DateTime)
 
-    def __init__(self, name, provenance, format, date_added, last_run):
+    def __init__(self, name, datatype_id, provenance, format, date_added, last_run):
         self.name = name
+        self.datatype_id = datatype_id
         self.provenance = provenance
         self.format = format
         self.date_added = date_added
         self.last_run = last_run
 
     def __repr__(self):
-        return f"Network details: \n{self.name}\n{self.provenance}\n{self.format}\n{self.date_added}\n{self.last_run}"
+        return f"Network details: \n{self.name}\n{self.datatype_id}\n{self.provenance}\n{self.format}\n{self.date_added}\n{self.last_run}"
 
-    def display_datatypes(self):
-        print("Datatypes")
-        for dt in self.datatype:
-            print(dt.name)
+    # def display_datatypes(self):
+    #     print("Datatypes")
+    #     for dt in self.datatype:
+    #         print(dt.name)
 
 
 class Datatype(db.Model):
@@ -84,11 +86,12 @@ class Datatype(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.Text)
-    network_id = db.Column(db.Integer, db.ForeignKey("networks.id"))
+    # ONE TO MANY
+    datatype = db.relationship("Network", backref="datatype", lazy="dynamic")
+    # network_id = db.Column(db.Integer, db.ForeignKey("networks.id"))
 
-    def __init__(self, name, network_id):
+    def __init__(self, name):
         self.name = name
-        self.network_id = network_id
 
 
 # @app.route("/")
