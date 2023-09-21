f108656c504c9174750c0b759eb48e541593000f
Madeleine Dunn <madeleine.dunn@arm.com>
2023-09-21 18:41:30 +0100

    Add kickoff run view/form/template

--- web_application/__init__.py
+++ web_application/__init__.py
@@ -6,16 +6,16 @@ from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 
-# create a login manager
+# Create a login manager
 login_manager = LoginManager()
 
-# set up flask application
+# Set up flask application
 app = Flask(__name__)
 
-# secret key for forms
+# Secret key for forms
 app.config["SECRET_KEY"] = "mysecretkey"
 
-# database setup and config
+# Database setup and config
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(
@@ -26,19 +26,21 @@ app.config["SQL_ALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 Migrate(app, db)
 
-# initialise login manager using the app and set the login page
+# Initialise login manager using the app and set the login page
 login_manager.init_app(app)
 login_manager.login_view = "login"
 
 
-# imports for flask blueprints
+# Imports for flask blueprints
 from web_application.handlers import error_blueprint
 from web_application.views.authentication_view import authentication_blueprint
 from web_application.views.device_view import device_blueprint
+from web_application.views.kickoff_run_view import kickoff_blueprint
 from web_application.views.network_view import network_blueprint
 
-# blueprints
+# Blueprints
 app.register_blueprint(authentication_blueprint, url_prefix="/auth")
 app.register_blueprint(device_blueprint, url_prefix="/device")
 app.register_blueprint(error_blueprint, url_prefix="/error")
+app.register_blueprint(kickoff_blueprint, url_prefix="/kickoff")
 app.register_blueprint(network_blueprint, url_prefix="/network")
